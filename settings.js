204b3be9163530f4ed309a5f55ab4d002ce94784
gaming akiya <115622420+akiyaboy@users.noreply.github.com>
2022-10-13 14:45:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,32 +54,32 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['94767973534'] //ur owner number
+global.ownername = "☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠" //ur owner name
+global.ytname = "YT: gaming akiya" //ur yt chanel name
+global.socialm = "GitHub: akila sandaruwan" //ur github or insta name
+global.location = "sri lanka, bandarawela, katugaha" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.owner = ['94767973534']
+global.ownertag = '94767973534' //ur tag number
+global.botname = '☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠' //ur bot name
+global.linkz = "https://chat.whatsapp.com/DwvNpjxS8K9IJ9OSgvS5wz" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/channel/UC69O34ynMDdjp4Nd4ntGqEQ" //ur website to be displayed
+global.botscript = 'https://youtube.com/channel/UCYqsy4l8eHh0kFVF6IUSuiQ' //script link
+global.reactmoji = "🤡" //ur menu react emoji
+global.themeemoji = "🤡" //ur theme emoji
+global.packname = "☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠" //ur sticker watermark packname
+global.author = "☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠" //ur sticker watermark author
 
 //Bot theme media
-global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
-global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
-global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
-global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.thum = fs.readFileSync("https://telegra.ph/file/cc55f88d161978570f55e.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("https://telegra.ph/file/cc55f88d161978570f55e.jpg") //ur logo pic
+global.err4r = fs.readFileSync("https://telegra.ph/file/cc55f88d161978570f55e.jpg") //ur error pic
+global.thumb = fs.readFileSync("https://telegra.ph/file/cc55f88d161978570f55e.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['94767973534'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -105,20 +105,20 @@ global.antitags = true
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
-    group: 'Feature Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot',
-    wait: 'In process...',
-    linkm: 'Where is the link?',
-    error: 'Error!!',
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
-    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
-    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
-    banChat: 'The bot was banned in this group, please contact the owner to unban'
+    success: 'Done ✓ ☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠',
+    admin: 'This Feature Is Only For Admin! ☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠',
+    botAdmin: 'Bot Must Be Admin First! ☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠',
+    owner: 'This Feature Is Only For Owner! ☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠',
+    group: 'Feature Used Only For Groups! ☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠',
+    private: 'Features Used Only For Private Chat! ☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠',
+    bot: 'This Feature Is Only For Bot ☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠',
+    wait: 'In process...☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠',
+    linkm: 'Where is the link? ☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠',
+    error: 'Error!! ☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours ☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠',
+    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate ☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠',
+    banChat: 'The bot was banned in this group, please contact the owner to unban ☬K༙྇I༙྇N༙྇G༙྇ᴼᶠ𝙹𝙾𝙲𝙺𝙴𝚁〠'
 }
     global.limitawal = {
     premium: "Infinity",
